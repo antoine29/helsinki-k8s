60f85af20e56ace8cb4ca319250547bc4b499126
Anthony Flores <floresgomezanthony@gmail.com>
2022-08-11 19:00:51 -0400

    adding todo fe

---
+++ part1/4.web-server/todo-fe/src/lib/ToDoList.svelte
@@ -0,0 +1,41 @@
+<script>
+    let newItem = "";
+
+    let todoList = [
+        { text: "Write my first post", status: true },
+        { text: "Upload the post to the blog", status: false },
+        { text: "Publish the post at Facebook", status: false },
+    ];
+
+    function addToList() {
+        todoList = [...todoList, { text: newItem, status: false }];
+        newItem = "";
+    }
+
+    function removeFromList(index) {
+        todoList.splice(index, 1);
+        todoList = todoList;
+    }
+</script>
+
+<div class="list">
+    <input bind:value={newItem} type="text" placeholder="new todo item.." />
+    <button on:click={addToList}>Add</button>
+
+    <br />
+    {#each todoList as item, index}
+        <input bind:checked={item.status} type="checkbox" />
+        <span class:checked={item.status}>{item.text}</span>
+        <span on:click={() => removeFromList(index)}>❌</span>
+        <br />
+    {/each}
+</div>
+
+<style>
+    .checked {
+        text-decoration: line-through;
+    }
+    .list {
+        text-align: left;
+    }
+</style>
