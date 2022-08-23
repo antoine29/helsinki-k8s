96abf1d9b74a8f3f45fc068c9469ed8ea15e0886
antoine <anthonyfg9@gmail.com>
2022-08-23 17:35:12 -0400

    adding timeStamp and file writing for ping-pong

--- part1/PingPongApp/main.go
+++ part1/PingPongApp/main.go
@@ -11,5 +11,6 @@ func main() {
 		fmt.Println("Warning: 'GO_PORT' environment variable was not set, using 8080 as default.")
 		port = "8080"
 	}
+	
 	Server(port)
 }
--- part1/PingPongApp/server.go
+++ part1/PingPongApp/server.go
@@ -3,17 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var counter int = 0
 
-func current(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, counter)
+func getCurrentStatus() string {
 	counter++
+	timeStamp := time.Now()
+	status := fmt.Sprintf("time: %s \t counter: %d\n", timeStamp, counter)
+	return status
+}
+
+func statusController(w http.ResponseWriter, req *http.Request) {
+	status := getCurrentStatus()
+	WriteToFile(status)
+	fmt.Println(req.RequestURI)
+	fmt.Fprintln(w, status)
 }
 
 func Server(port string) {
-	http.HandleFunc("/", current)
+	http.HandleFunc("/", statusController)
 
 	fmt.Printf("Listening on: %s \n", port)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
