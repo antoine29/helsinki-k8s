e8e4d6a40c6f6ad19bdbff03073ac77eab9c30d0
antoine <anthonyfg9@gmail.com>
2022-08-27 20:59:04 -0400

    project: adding daily image

--- part1/4.web-server(project)/todo-fe/src/App.svelte
+++ part1/4.web-server(project)/todo-fe/src/App.svelte
@@ -2,18 +2,34 @@
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
   import ToDoList from './lib/ToDoList.svelte';
+
+  const getDailyPicNumber = () => {
+    const date = new Date();
+    const number = `${date.getDate()}${date.getMonth()+1}`
+    return number
+  }
+
+  const apiUrl = import.meta.env.VITE_API_URL
+
+  const click = (event) => {console.log(apiUrl)}
 </script>
 
-<main>
-  <div>
+
+<main class="container">
+  <img
+    src={`${apiUrl}/image/${getDailyPicNumber()}`}
+    alt="Vite Logo"
+    on:click={click} />
+ 
+  <h1>Go + Vite + Svelte + TS => ToDo list</h1>
+  <!-- <div >
     <a href="https://vitejs.dev" target="_blank"> 
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank"> 
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  </div> -->
 
   <!-- <div class="card">
     <Counter />
@@ -42,7 +58,12 @@
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
-  .read-the-docs {
+  /* .read-the-docs {
     color: #888;
-  }
+  } */
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;  }
 </style>
