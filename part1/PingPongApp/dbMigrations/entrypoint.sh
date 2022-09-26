eb78765aeab7752e2ad8f32490406c821f833a20
antoine <anthonyfg9@gmail.com>
2022-09-26 13:02:11 -0400

    PingPong: adding liquibse migrations

---
+++ part1/PingPongApp/dbMigrations/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/liquibase/liquibase --log-level debug --classpath="/" --url "jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DBNAME}?ssl=false" --username="${PG_USERNAME}" --password="${PG_PASSWORD}" --changelog-file="${CHANGE_LOG}" --hub-mode=off update
