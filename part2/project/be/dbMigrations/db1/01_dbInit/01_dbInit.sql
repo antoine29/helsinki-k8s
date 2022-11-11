47d814dce3225fba18787e4e52bcb28d7ad1d3c7
antoine <anthonyfg9@gmail.com>
2022-11-11 00:41:33 -0400

    project: be setting todo content as a text column type & addind todo pending stuff

--- part2/project/be/dbMigrations/db1/01_dbInit/01_dbInit.sql
+++ part2/project/be/dbMigrations/db1/01_dbInit/01_dbInit.sql
@@ -2,6 +2,6 @@ CREATE SCHEMA IF NOT EXISTS todo;
 
 CREATE TABLE IF NOT EXISTS todo.todos (
     id            varchar(40) CONSTRAINT firstkey PRIMARY KEY,
-    content       varchar(40) NOT NULL,
+    content       text NOT NULL,
     is_done       boolean NOT NULL
 );
---
+++ part2/project/readme.md
@@ -0,0 +1,12 @@
+# ToDo
+
+Full stack app
+Golang + Postgres + Vite
+
+Deployed as kubernetes
+
+## ToDo:
+
+- fix be health endpoint. it should be `be-svc/health` instead `be-svc/api/health`
+- fix swagger docs in k8s
+
