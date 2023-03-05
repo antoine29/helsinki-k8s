f634ce8c379251d9c5fec33656499360c476f84f
Anthony Flores <floresgomezanthony@gmail.com>
2023-03-05 16:59:02 -0400

    Exercise: 4.05: Project v1.9 adding put todo method and updating swager docs

--- part2/project/be/src/router/router.go
+++ part2/project/be/src/router/router.go
@@ -31,7 +31,7 @@ func SetupServer() *gin.Engine {
 		api.GET("/todos", controllers.GetTodos)
 		api.GET("/todos/:id", controllers.GetTodo)
 		api.POST("/todos", controllers.PostTodo)
-		api.PUT("/todos", controllers.PostTodo)
+		api.PUT("/todos/:id", controllers.PutTodo)
 		api.PATCH("/todos/:id", controllers.UpdateTodo)
 		api.DELETE("/todos/:id", controllers.DeleteTodo)
 		api.GET("/health", controllers.HealthCheck)
