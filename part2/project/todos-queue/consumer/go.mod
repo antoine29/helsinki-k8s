90f6c56b13093d80107286cfd2883c1fa8a60c67
Anthony Flores <floresgomezanthony@gmail.com>
2023-03-17 19:28:45 -0400

    project: cleaning queue consumer

--- part2/project/todos-queue/consumer/go.mod
+++ part2/project/todos-queue/consumer/go.mod
@@ -2,11 +2,19 @@ module github.com/antoine29/todos-queue-consumer
 
 go 1.19
 
-require github.com/nats-io/nats.go v1.24.0
+require (
+	github.com/antoine29/todos-queue-telegram-client v0.0.0-00010101000000-000000000000
+	github.com/joho/godotenv v1.5.1
+	github.com/nats-io/nats.go v1.24.0
+)
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/nats-io/nats-server/v2 v2.9.15 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
+
+replace github.com/antoine29/todos-queue-telegram-client => ../telegram-client
--- part2/project/todos-queue/consumer/pkg/natsClient/consumer.go
+++ part2/project/todos-queue/consumer/pkg/natsClient/consumer.go
@@ -2,11 +2,23 @@ package natsClient
 
 import (
 	"log"
+	"os"
 
+	tg "github.com/antoine29/todos-queue-telegram-client/pkg"
 	nats "github.com/nats-io/nats.go"
+  _ "github.com/joho/godotenv/autoload"
 )
 
+var TG_BOT_TOKEN, isTgBotTokenEnvVarSet = os.LookupEnv("TG_BOT_TOKEN")
+var TG_CHANNEL_NAME, isTgChannelNameEnvVarSet = os.LookupEnv("TG_CHANNEL_NAME")
+var NATS_URL, isNatsUrlEnvVarSet = os.LookupEnv("NATS_URL")
+
 func Subscribe(subject string) {
+  if !isNatsUrlEnvVarSet {
+    log.Println("'NATS_URL' env var not set. Exiting.")
+    return
+  }
+
 	nconn, err := nats.Connect(nats.DefaultURL)
 	defer nconn.Close()
 	if err != nil {
@@ -14,16 +26,29 @@ func Subscribe(subject string) {
 		return
 	}
 
-	nchan := make(chan *nats.Msg, 64)
-	nsub, err := nconn.ChanSubscribe(subject, nchan)
+	natsChannel := make(chan *nats.Msg, 64)
+	nsub, err := nconn.ChanSubscribe(subject, natsChannel)
 	defer nsub.Unsubscribe()
 	if err != nil {
-		log.Printf("Error connecting to subscribing\n%s\n", err.Error())
+		log.Printf("Error subscribing to nats subject. \n%s\n", err.Error())
 		return
 	}
 
 	log.Println("Subscribed to", subject)
-	for nmsg := range nchan {
-		log.Println(string(nmsg.Data))
-	}
+	for natsMsg := range natsChannel {
+    msg := string(natsMsg.Data)
+    log.Println("Message received:", msg)
+    sendMsgToTg(msg)
+  }
 }
+
+func sendMsgToTg(message string) {
+  if isTgBotTokenEnvVarSet && isTgChannelNameEnvVarSet {
+    if err := tg.SendTgMessageFromBotToChannel(TG_BOT_TOKEN, TG_CHANNEL_NAME, message); err != nil {
+      log.Println(err.Error())
+    }
+  } else {
+    log.Println("TG env vars not set")
+  }
+}
+
